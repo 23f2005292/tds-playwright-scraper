7816566d9a5ed28fd8fb0536a7538c3a8b6ccf66
23f2005292@ds.study.iitm.ac.in <samudra@LAPTOP-FN1L1CR5.localdomain>
2026-02-28 04:50:52 +0000

    Fix CI chromium permissions

--- scrape.js
+++ scrape.js
@@ -1,7 +1,10 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
   const page = await browser.newPage();
 
   let grandTotal = 0;
